f1dbbd4321829c29ada79b5797d1ed583c63efc3
Quynh Cao Thi Thanh <caothanhquynh07012003@gmail.com>
2026-06-30 11:39:15 +0700

    create WebDriver initialization and teardown utility

---
+++ src/test/java/Dropdown/HandleCheckbox.java
@@ -0,0 +1,10 @@
+package Dropdown;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandleCheckbox {
+    public static void main(String[] args) {
+        //Inittial
+        WebDriver driver = new ChromeDriver();
+    }
+}
---
+++ src/test/java/Initialization/Init.java
@@ -0,0 +1,37 @@
+package Initialization;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Init {
+    //Declare the WebDriver instance
+    private WebDriver driver;
+
+    /**
+     * Initialize the Chrome browser and configure Webdriver settings
+     */
+    public void setup() {
+        //Lauch Chrome driver
+        driver = new ChromeDriver();
+        //Wait up to 30 seconds for a page to finish loading
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        //Set implicit wait to 20 seconds for locating elements
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        //Maximize the browser window
+        driver.manage().window().maximize();
+    }
+
+    /**
+     * Close the browser and end the Webdriver session
+     *
+     * @throws InterruptedException
+     */
+    public void tearDown() throws InterruptedException {
+        //Close all browser windows and terminate the Webdriver session
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
