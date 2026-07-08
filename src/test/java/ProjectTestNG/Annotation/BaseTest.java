9caecc8c9fc5f801df26dc374fe526204558fbac
Quynh Cao Thi Thanh <caothanhquynh07012003@gmail.com>
2026-07-08 16:33:38 +0700

    Annotation khi có kế thừa

---
+++ src/test/java/ProjectTestNG/Annotation/BaseTest.java
@@ -0,0 +1,28 @@
+package ProjectTestNG.Annotation;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    @BeforeClass
+    public void beforeBaseClass() {
+        System.out.println("Parent Before Class method");
+    }
+
+    @AfterClass
+    public void afterBaseClass() {
+        System.out.println("Parent After Class method");
+    }
+
+    @BeforeMethod
+    public void beforeBaseMethod() {
+        System.out.println("Parent Before method");
+    }
+
+    @AfterMethod
+    public void afterBaseMethod() {
+        System.out.println("Parent After method");
+    }
+}
---
+++ src/test/java/ProjectTestNG/Annotation/TestClass.java
@@ -0,0 +1,31 @@
+package ProjectTestNG.Annotation;
+
+import org.testng.annotations.*;
+
+public class TestClass extends BaseTest{
+    @BeforeClass
+    public void beforeChildClass() {
+        System.out.println("Child Before Class method");
+    }
+
+    @AfterClass
+    public void afterChildClass() {
+        System.out.println("Child After Class method");
+    }
+
+    @BeforeMethod
+    public void beforeChildMethod() {
+        System.out.println("Child Before method");
+    }
+
+    @AfterMethod
+    public void afterChildMethod() {
+        System.out.println("Child After method");
+    }
+
+    @Test
+    public void testMethod() {
+        System.out.println("Test method under TestClass");
+    }
+
+}
